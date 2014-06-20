3d5aee06761cb12f4e39ffa326dbc0c7dd8ad7f1
vagrant <vagrant@vagrant-centos63.(none)>
2014-06-20 07:01:16 +0100

    create following models
    * Neco
    * Tag
    * Category

--- necommend/models.py
+++ necommend/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Tag(models.Model):
+  name = models.CharField(max_length=200)
+ 
+class Category(models.Model):
+  name = models.CharField(max_length=200)
+
+class Neco(models.Model):
+  img = models.CharField(max_length=200)
+  likes = models.IntegerField()
+  category = models.ForeignKey(Category)
+  tags = models.ManyToManyField(Tag)
+
